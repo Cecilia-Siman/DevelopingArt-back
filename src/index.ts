b25a210229d4a27363801885b7abf07f8632a0ba
Cecilia Salema <ceciliasiman15@gmail.com>
2022-10-07 19:00:54 -0300

    feat app and index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import app from './app';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
